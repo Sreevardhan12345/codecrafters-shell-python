68517653c6ed9283783d9938eccfaaf3ee7e324d
TULLURI SREE VARDHAN <TULLURISREEVARDHAN@GMAIL.COM>
2026-07-18 17:51:38 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -9,6 +9,14 @@ def main():
             sys.exit(0)
         elif command.startswith("echo "):
             sys.stdout.write( command[5:] + "\n")
+        elif command == "type":
+            cmdLets = command.split()
+            if len(cmdLets) > 1:
+                if cmdLets[1] in ["echo", "type", "exit"]:
+                    sys.stdout.write(f"{cmdLets[1]} is a shell builtin\n")
+                sys.stdout.write(f"{cmdLets[1]} is /usr/bin/{cmdLets[1]}\n")
+            else:
+                sys.stdout.write("type: missing operand\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
     pass
