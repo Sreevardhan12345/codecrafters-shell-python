180f62b6f2ccda48e2d4477211e29deceedec90c
TULLURI SREE VARDHAN <TULLURISREEVARDHAN@GMAIL.COM>
2026-07-18 17:46:31 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
         if command == "exit":
             sys.exit(0)
         elif command.startswith("echo "):
-            sys.stdout.write(command + "\n")
+            sys.stdout.write("$ "+ command + "\n")
         sys.stdout.write(f"{command}: command not found\n")
     pass
 
